6dcec61a4de644846b25764ce8726c0797ba606d
Raghad0212 <raghadfarrah123@gmail.com>
2025-08-29 21:02:21 +0300

    added the player animation run,jump

--- scripts/objects/coin.gd
+++ scripts/objects/coin.gd
@@ -3,7 +3,7 @@ extends Area2D
 @onready var anim_player : AnimationPlayer= get_node("AnimationPlayer")
 func _on_body_entered(body: PhysicsBody2D) ->void:
 	anim_player.play("fade out")
-	
+	Global.score +=1
 
 
 
